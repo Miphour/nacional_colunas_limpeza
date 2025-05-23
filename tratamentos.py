ae1118072f660411fe6218f8078f1198491aba3b
Miphour <143912540+Miphour@users.noreply.github.com>
2025-05-23 19:16:34 -0300

    Update tratamentos.py

--- tratamentos.py
+++ tratamentos.py
@@ -29,6 +29,27 @@ def trata_cod_cartorio(planilha):
     planilha[alvo] = planilha[alvo].str.zfill(2)
   return planilha
 
+def trata_uf_cra(planilha):
+  alvo = 'UF da CRA'
+  if alvo in planilha.columns:
+    planilha[alvo] = planilha[alvo].zfill(2)
+    planilha[alvo] = planilha[alvo].astype(str)
+  return planilha
+
+def trata_uf_sacador(planilha):
+  alvo = 'UF do Sacador'
+  if alvo in planilha.columns:
+    planilha[alvo] = planilha[alvo].zfill(2)
+    planilha[alvo] = planilha[alvo].astype(str)
+  return planilha
+
+def trata_cod_ibge(planilha):
+  alvo = 'Código IBGE da Pç Pagto'
+  if alvo in planilha.columns:
+    planilha[alvo] = planilha[alvo].zfill(7)
+    planilha[alvo] = planilha[alvo].astype(str)
+  return planilha
+
 
 def tratamento_padrao(planilha):
   planilha = trata_documento_devedor(planilha)
