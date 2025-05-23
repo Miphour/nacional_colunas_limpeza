aa68fc41dc61972fae62316e3bf1ca9a4e0a71f9
Miphour <143912540+Miphour@users.noreply.github.com>
2025-05-23 18:38:00 -0300

    Update tratamentos.py

--- tratamentos.py
+++ tratamentos.py
@@ -13,8 +13,24 @@ def trata_documento_sacador(planilha):
     planilha[alvo] = planilha[alvo].str.zfill(14)
   return planilha
 
+def trata_cod_portador(planilha):
+  alvo = "Código do Portador"
+    planilha[alvo] = planilha[alvo].astype(str)
+    planilha[alvo] = planilha[alvo].str.zfill(3)
+  return planilha
+
+def trata_cod_cartorio(planilha):
+  alvo = 'Código do Cartório'
+    base[alvo] = base[alvo].fillna(0)
+    base[alvo] = base[alvo].astype(int).astype(str)
+    base[alvo] = base[alvo].str.zfill(2)
+return planilha
+
+
 def tratamento_padrao(planilha):
   planilha = trata_documento_devedor(planilha)
   planilha = trata_documento_sacador(planilha)
+  planilha = trata_cod_portador(planilha)
+  planilha = trata_cod_cartorio(planilha)
   
   return planilha
