ce5dd0abd67dbbb01512688a7e7d7a10ee0cd109
Miphour <143912540+Miphour@users.noreply.github.com>
2025-05-23 18:23:25 -0300

    Create tratamentos.py
    teste 1

---
+++ tratamentos.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def trata_documento_devedor(planilha):
+  if 'Documento do Devedor' in planilha.columns:
+    planilha['Documento do Devedor'] = planilha['Documento do Devedor'].astype(str)
+    planilha['Documento do Devedor'] = planilha['Documento do Devedor'].str.zfill(14)
+  return planilha
+
+def trata_documento_sacador(planilha):
+  alvo = 'Documento do Sacador'
+  if alvo in planilha.columns:
+    planilha[alvo] = planilha[alvo].astype(str)
+    planilha[alvo] = planilha[alvo].str.zfill(14)
+  return planilha
